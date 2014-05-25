6d0af1c8e3fca84db00b65091149ae844544549d
leon_huang <leon_huang@gemteks.com>
2014-05-25 11:31:44 +0800

    create Table View Controller with .xib (p.129-134)
    New File > iOS > Cocoa Touch > Objective-C class
    
    Class: MainTableViewController
    
    Subclass of: UITableViewController
    
    Also create XIB file (iPhone)

---
+++ ios-core-data-demo/MainTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainTableViewController.h
+//  ios-core-data-demo
+//
+//  Created by zlargon on 2014/5/25.
+//  Copyright (c) 2014年 zlargon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainTableViewController : UITableViewController
+
+@end
